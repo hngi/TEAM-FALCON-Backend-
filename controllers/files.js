ebfdda1eb5c8758aba63925fd2eb4debde527833
ajibadeabd <ajibadeabd@gmail.com>
2020-06-08 22:17:32 +0100

    created an updated route

--- controllers/files.js
+++ controllers/files.js
@@ -1,4 +1,4 @@
-
+const File = require('../models/file.model')
 /**
  * Controllers for :
  * 
@@ -9,7 +9,28 @@
  * deleteFile
  */
 class FileContoller {
-     
+   async updateFile(req, res, next) {
+                await   File.findOneAndUpdate(
+                    {_id:req.params.fileId} 
+                    ,req.body,
+        
+                    {new:true},
+                    (err,id)=>{
+                            if(err) return res.status(404).json({
+                                error:err,
+                                status:false,
+                            });
+                            res.json(
+                            {
+                                meesage:'file updated',
+                                meesage:true,
+                                
+                            }
+                             ) 
+                    });
+              
+              
+                  }
 
 }
 
